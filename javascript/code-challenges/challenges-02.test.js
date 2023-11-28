d2e638a579684985bf2e8d7ef83e73da64401a04
KAITLIN DAVIS <KAITLIN.A.DAVIS1996@GMAIL.COM>
2023-11-28 16:41:37 -0600

    Completed challenge 2

--- javascript/code-challenges/challenges-02.test.js
+++ javascript/code-challenges/challenges-02.test.js
@@ -26,7 +26,9 @@ Write a function named addOne that, given an array of numbers, uses map to retur
 ------------------------------------------------------------------------------------------------ */
 
 const addOne = (arr) => {
-  // Solution code here...
+  return arr.map((number) => {
+    return number + 1;
+  });
 };
 
 /* ------------------------------------------------------------------------------------------------
