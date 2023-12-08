f5f4bc11a11d96498f59e1e2c606695303251772
KAITLIN DAVIS <KAITLIN.A.DAVIS1996@GMAIL.COM>
2023-12-08 00:23:29 -0600

    completed 4

--- javascript/code-challenges/challenges-09.test.js
+++ javascript/code-challenges/challenges-09.test.js
@@ -75,8 +75,9 @@ console.log(a) prints { fullName: 'Octavia Estelle Butler', yearBorn: 1947 }
 ------------------------------------------------------------------------------------------------ */
 
 const addBirthYearProperty = (obj, year) => {
-  // Solution code here...
+  obj.yearBorn = year;
 };
+
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 5 - Stretch Goal
