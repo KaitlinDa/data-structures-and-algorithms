a3cd746c281f27ca12a2e325699af5037ae2b91c
KaitlinDa <Kaitlin.A.Davis1996@Gmail.com>
2024-02-01 10:02:43 -0600

    added function for code challenge 18

--- python/code_challenges/tree_fizz_buzz.py
+++ python/code_challenges/tree_fizz_buzz.py
@@ -1,5 +1,47 @@
 from data_structures.binary_tree import BinaryTree
 
 
-def fizz_buzz_tree():
-    pass
+class Node:
+    def __init__(self, value=None):
+        self.value = value
+        self.children = []  
+
+class KaryTree:
+    def __init__(self, root=None):
+        self.root = root
+
+    def breadth_first(self):
+        """A method to return the values of the tree nodes in breadth-first order."""
+        values = []
+        queue = [self.root] if self.root else []
+
+        while queue:
+            current = queue.pop(0)
+            values.append(str(current.value))
+            for child in current.children:
+                queue.append(child)
+
+        return values
+
+def fizz_buzz(value):
+    """Helper function to transform the value based on divisibility."""
+    if value % 3 == 0 and value % 5 == 0:
+        return "FizzBuzz"
+    elif value % 3 == 0:
+        return "Fizz"
+    elif value % 5 == 0:
+        return "Buzz"
+    else:
+        return str(value)
+
+def fizz_buzz_tree(kary_tree):
+    """Transforms the k-ary tree values based on the FizzBuzz rules."""
+    def clone_tree(node):
+        if node is None:
+            return None
+        new_node = Node(fizz_buzz(node.value))
+        new_node.children = [clone_tree(child) for child in node.children]
+        return new_node
+
+    return KaryTree(clone_tree(kary_tree.root))
+
---
+++ python/docs/tree_fizz_buzz/README.md
@@ -0,0 +1,16 @@
+# Code Challenge 07 - Class 401d24
+
+## Author
+Kaitlin Davis | January 2024
+
+## Challenge Title
+Tree Fizz Buzz
+
+## Whiteboard Process
+![Whiteboard for Tree Breadth First](code18.png)
+
+## Approach & Efficiency
+
+
+## Resources
+I used ChatGPT for help with this assignment. 
