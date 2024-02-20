38751e126677aaf6b5830be092e8a711395f5600
KaitlinDa <Kaitlin.A.Davis1996@Gmail.com>
2024-02-20 08:51:16 -0600

    finishes code challenge 31

--- python/code_challenges/hashtable_repeated_word.py
+++ python/code_challenges/hashtable_repeated_word.py
@@ -1,5 +1,19 @@
 from data_structures.hashtable import Hashtable
+import re
+
+def first_repeated_word(s):
+    s = s.lower()
+    
+    words = re.findall(r'\b\w+\b', s)
+    
+    word_set = set()
+    
+    for word in words:
+        if word in word_set:
+            return word
+        word_set.add(word)
+    
+    return None
 
 
-def first_repeated_word():
-    pass
+
