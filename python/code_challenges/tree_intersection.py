9816f80020d99dd63292bac187273a13ed3d8c18
KaitlinDa <Kaitlin.A.Davis1996@Gmail.com>
2024-02-21 08:51:30 -0600

    finishes challenge 32

--- python/code_challenges/tree_intersection.py
+++ python/code_challenges/tree_intersection.py
@@ -1,5 +1,20 @@
 from data_structures.binary_tree import BinaryTree
 
 
-def tree_intersection():
-    pass
+def tree_intersection(tree_a, tree_b):
+    def traverse_and_collect_values(tree):
+        values = set()
+        def traverse(node):
+            if node:
+                values.add(node.value)
+                traverse(node.left)
+                traverse(node.right)
+        traverse(tree.root)
+        return values
+    
+    values_tree_a = traverse_and_collect_values(tree_a)
+    values_tree_b = traverse_and_collect_values(tree_b)
+    
+    intersection = values_tree_a.intersection(values_tree_b)
+    
+    return list(intersection)
