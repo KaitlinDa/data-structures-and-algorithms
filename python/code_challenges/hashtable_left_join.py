3f3890581de018245a3753fa8a69771dc22e82d3
KaitlinDa <Kaitlin.A.Davis1996@Gmail.com>
2024-02-22 10:14:42 -0600

    finishes code challenge 33

--- python/code_challenges/hashtable_left_join.py
+++ python/code_challenges/hashtable_left_join.py
@@ -1,5 +1,16 @@
 from data_structures.hashtable import Hashtable
 
 
-def left_join():
-    pass
+def left_join(synonyms, antonyms):
+    result = []
+    
+    for key, value in synonyms.items():
+        if key in antonyms:
+            antonym = antonyms[key]
+        else:
+            antonym = "NONE"
+        
+        result.append([key, value, antonym])
+    
+    return result
+
